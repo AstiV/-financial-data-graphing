7078e4239ee6ffe9f2bdf209845895d3f0c7884a
Astrid Varga <astrid.varga@gmx.de>
2018-09-01 15:02:18 +0200

    Add responsive CSS style to Canvas Graph

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
+let apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+drawChart(apiUrl);
 
-  console.log('IronGenerator JS imported successfully!');
+$("input").on("change", function() {
+    let start = $("#start").val();
+    let end = $("#end").val();
+    if (start && end) {
+        apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json?start=" + start + "&end=" + end;
+        drawChart(apiUrl);
+    }
+});
 
-}, false);
+function drawChart(apiUrl) {
+    axios.get(apiUrl).then(result => {
+        // console.log(Object.keys(result.data.bpi), Object.values(result.data.bpi));
+        const dates = Object.keys(result.data.bpi);
+        const values = Object.values(result.data.bpi);
+
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: dates,
+                datasets: [
+                    {
+                        label: "# of Votes",
+                        data: values,
+                        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+                        borderColor: ["rgba(255,99,132,1)"],
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                responsive: true
+            }
+        });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
+
+// axios.get("https://api.iextrading.com/1.0/stock/amzn/chart").then(result => {
+//     console.log(result.data);
+//     const days = result.data.map(el => el.label);
+//     const values = result.data.map(el => el.close);
+//     var ctx = document.getElementById("myChart").getContext("2d");
+//     var myChart = new Chart(ctx, {
+//         type: "line",
+//         data: {
+//             labels: days,
+//             datasets: [
+//                 {
+//                     label: "Amazon Stock Data",
+//                     data: values,
+//                     backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+//                     borderColor: ["rgba(255,99,132,1)"],
+//                     borderWidth: 1
+//                 }
+//             ]
+//         },
+//         options: {}
+//     });
+// });
